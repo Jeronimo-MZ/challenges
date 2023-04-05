72c90e692c219b36095a0f3061e53b628b03c3a6
Jeronimo-MZ <mataveljeronimo@gmail.com>
2023-04-05 12:32:51 +0200

    feat(bora-codar/credit-card-form): sync form with card

--- bora-codar/13-credit-card-form/script.js
+++ bora-codar/13-credit-card-form/script.js
@@ -1,13 +1,40 @@
-var cardNumberInput = document.getElementById("card-number");
-var expiryDateInput = document.getElementById("card-expiry-date");
-var cvvInput = document.getElementById("card-cvv");
+const cardNumberInput = document.getElementById("card-number");
+const cardOwnerInput = document.getElementById("card-owner");
+const expiryDateInput = document.getElementById("card-expiry-date");
+const cvvInput = document.getElementById("card-cvv");
 
-IMask(cardNumberInput, { mask: "0000 0000 0000 0000" });
+const cardNumberText = document.querySelector(".credit-card .card-number");
+const cardOwnerText = document.querySelector(".credit-card .card-owner");
+const expiryDateText = document.querySelector(".credit-card .card-expiry-date");
+
+const cardNumberMask = IMask(cardNumberInput, {
+    lazy: false,
+    placeholderChar: "*",
+    mask: "0000 0000 0000 0000",
+});
 IMask(expiryDateInput, {
     mask: "MM/YY",
+    lazy: false,
     blocks: {
-        YY: { mask: "00" },
-        MM: { mask: IMask.MaskedRange, from: 1, to: 12 },
+        YY: { mask: "00", placeholderChar: "a" },
+        MM: {
+            mask: IMask.MaskedRange,
+            placeholderChar: "m",
+            from: 1,
+            to: 12,
+        },
     },
 });
 IMask(cvvInput, { mask: "000" });
+
+cardNumberInput.addEventListener("keyup", (event) => {
+    cardNumberText.innerHTML = event.target.value;
+});
+
+cardOwnerInput.addEventListener("keyup", (event) => {
+    cardOwnerText.innerHTML = event.target.value;
+});
+
+expiryDateInput.addEventListener("keyup", (event) => {
+    expiryDateText.innerHTML = event.target.value;
+});
